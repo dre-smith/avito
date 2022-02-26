4a184cef97bfab91be9889056d333bebb24326cd
Andrey <steamprofile2020@gmail.com>
2022-02-27 00:04:42 +0300

    Vuex: data transfer to storage

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -36,33 +36,10 @@ export default {
   components: {
     NavItem,
   },
-  data() {
-    return {
-      links: {
-        footer: {
-          id: 0,
-          link: "/",
-          icon: "Logo_black.svg",
-        },
-        other: [
-          {
-            id: 1,
-            text: "Our coffee",
-            link: "/our-coffee",
-          },
-          {
-            id: 2,
-            text: "For your pleasure",
-            link: "/for-your-pleasure",
-          },
-          {
-            id: 3,
-            text: "Contact us",
-            link: "/contact-us",
-          },
-        ],
-      },
-    };
+  computed: {
+    links() {
+      return this.$store.getters["getFooterLinks"];
+    },
   },
 };
 </script>
