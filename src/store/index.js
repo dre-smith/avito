3dda261d7995130c6184ac610ce5629d0218e19b
Andrey <steamprofile2020@gmail.com>
2022-02-28 21:16:48 +0300

    Vuex: refactoring

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,18 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import links from "@/store/links";
+import bestsellers from "@/store/bestsellers";
+import coffee from "@/store/coffee";
+import goods from "@/store/goods";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
     links,
+    bestsellers,
+    coffee,
+    goods,
   },
 });
 
