19c25dd5ee2185d588e207510be962624f995c3f
github-for-webdev <steamprofile2020@gmail.com>
2022-03-02 21:10:39 +0300

    Vue: validation (the library: Vuelidate)

--- src/views/ContactUsView.vue
+++ src/views/ContactUsView.vue
@@ -23,7 +23,7 @@
 
             <form @submit.prevent="submit" action="#" class="mt-5">
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="name-input" class="mb-0">
                     Name
                     <span style="color: red">*</span>
@@ -31,16 +31,19 @@
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.name"
+                    v-model="v$.name.$model"
                     type="text"
                     class="form-control"
                     id="name-input"
                   />
+                  <span v-for="error in v$.name.$errors" :key="error.$uid">{{
+                    error.$message
+                  }}</span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="email-input" class="mb-0">
                     E-mail
                     <span style="color: red">*</span>
@@ -48,30 +51,36 @@
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.email"
+                    v-model="v$.email.$model"
                     type="email"
                     class="form-control"
                     id="email-input"
                   />
+                  <span v-for="error in v$.email.$errors" :key="error.$uid">{{
+                    error.$message
+                  }}</span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="phone-input" class="mb-0"> Phone </label>
                 </div>
                 <div class="col col-12 col-sm-9">
                   <input
-                    v-model="form.phone"
+                    v-model="v$.phone.$model"
                     type="tel"
                     class="form-control"
                     id="phone-input"
                   />
+                  <span v-for="error in v$.phone.$errors" :key="error.$uid">{{
+                    error.$message
+                  }}</span>
                 </div>
               </div>
 
               <div class="form-group row textarea">
-                <div class="col col-12 d-flex justify-content-center">
+                <div class="col col-12 d-flex justify-content-start">
                   <label for="pmessage" class="mb-3 mt-3 text-center">
                     Your message
                     <span style="color: red">*</span>
@@ -79,13 +88,16 @@
                 </div>
                 <div class="col col-12">
                   <textarea
-                    v-model="form.message"
+                    v-model="v$.message.$model"
                     class="form-control"
                     name="message"
                     id="message"
                     rows="5"
                     placeholder="Leave your comments here"
                   ></textarea>
+                  <span v-for="error in v$.message.$errors" :key="error.$uid">{{
+                    error.$message
+                  }}</span>
                 </div>
               </div>
 
@@ -105,23 +117,40 @@
 <script>
 import NavBarComponent from "@/components/NavBarComponent.vue";
 
+import useVuelidate from "@vuelidate/core";
+import { required, email, minLength, maxLength } from "@vuelidate/validators";
+
 export default {
   components: {
     NavBarComponent,
   },
-  data() {
+  setup() {
+    return { v$: useVuelidate() };
+  },
+  validations() {
     return {
-      form: {
-        name: "",
-        email: "",
-        phone: "",
-        message: "",
+      name: { required },
+      email: { required, email },
+      phone: {},
+      message: {
+        required,
+        minLength: minLength(5),
+        maxLength: maxLength(20),
       },
     };
   },
+  data() {
+    return {
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    };
+  },
   methods: {
-    submit() {
-      console.log(this.form);
+    async submit() {
+      const isFormCorrect = await this.v$.$validate();
+      if (!isFormCorrect) return;
     },
   },
 };
