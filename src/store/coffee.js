a621140178b0514cd9e70c0f12ef0a7b260af514
github-for-webdev <steamprofile2020@gmail.com>
2022-03-04 23:50:28 +0300

    Vue: search and filtering (option 1)

--- src/store/coffee.js
+++ src/store/coffee.js
@@ -1,62 +1,48 @@
 const coffee = {
   state: {
-    coffee: [
-      {
-        id: 0,
-        name: "Solimo Coffee Beans 2kg",
-        price: 10.73,
-        image: "coffee-1.jpg",
-      },
-      {
-        id: 1,
-        name: "Presto Coffee Beans 1kg",
-        price: 15.99,
-        image: "coffee-2.jpg",
-      },
-      {
-        id: 2,
-        name: "AROMISTICO Coffee 1kg",
-        price: 6.99,
-        image: "coffee-3.jpg",
-      },
-      {
-        id: 3,
-        name: "Solimo Coffee Beans 2kg",
-        price: 10.73,
-        image: "coffee-1.jpg",
-      },
-      {
-        id: 4,
-        name: "Presto Coffee Beans 1kg",
-        price: 15.99,
-        image: "coffee-2.jpg",
-      },
-      {
-        id: 5,
-        name: "AROMISTICO Coffee 1kg",
-        price: 6.99,
-        image: "coffee-3.jpg",
-      },
-    ],
+    coffee: [],
+    searchValue: "",
+    sortValue: "",
   },
   mutations: {
     setCoffeeData(state, data) {
       state.coffee = data;
+    },
+    setSearchValue(state, value) {
+      state.searchValue = value;
+    },
+    setSortValue(state, value) {
+      state.sortValue = value;
     },
   },
   actions: {
     setCoffeeData({ commit }, data) {
       commit("setCoffeeData", data);
     },
+    setSearchValue({ commit }, value) {
+      commit("setSearchValue", value);
+    },
+    setSortValue({ commit }, value) {
+      commit("setSortValue", value);
+    },
   },
   getters: {
     getCoffee(state) {
-      return state.coffee;
+      return state.coffee
+        .filter((item) =>
+          item.name.toLowerCase().includes(state.searchValue.toLowerCase())
+        )
+        .filter((item) =>
+          item.country.toLowerCase().includes(state.sortValue.toLowerCase())
+        );
     },
     getProductById(state) {
       return (id) => {
         return state.coffee.find((card) => card.id === +id);
       };
+    },
+    getSearchValue(state) {
+      return state.searchValue;
     },
   },
 };
--- src/views/OurCoffeeView.vue
+++ src/views/OurCoffeeView.vue
@@ -52,6 +52,7 @@
                 type="text"
                 placeholder="start typing here..."
                 class="shop__search-input"
+                v-model="searchValue"
               />
             </form>
           </div>
@@ -59,9 +60,15 @@
             <div class="shop__filter">
               <div class="shop__filter-label">Or filter</div>
               <div class="shop__filter-group">
-                <button class="shop__filter-btn">Brazil</button>
-                <button class="shop__filter-btn">Kenya</button>
-                <button class="shop__filter-btn">Columbia</button>
+                <button class="shop__filter-btn" @click="onSort('Brazil')">
+                  Brazil
+                </button>
+                <button class="shop__filter-btn" @click="onSort('Kenya')">
+                  Kenya
+                </button>
+                <button class="shop__filter-btn" @click="onSort('Columbia')">
+                  Columbia
+                </button>
               </div>
             </div>
           </div>
@@ -103,6 +110,14 @@ export default {
     coffee() {
       return this.$store.getters["getCoffee"];
     },
+    searchValue: {
+      set(value) {
+        this.$store.dispatch("setSearchValue", value);
+      },
+      get() {
+        return this.$store.getters["getSearchValue"];
+      },
+    },
   },
   mixins: [navigate],
   mounted() {
@@ -111,6 +126,11 @@ export default {
       .then((data) => {
         this.$store.dispatch("setCoffeeData", data);
       });
+  },
+  methods: {
+    onSort(value) {
+      this.$store.dispatch("setSortValue", value);
+    },
   },
 };
 </script>
